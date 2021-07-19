16d522c82c4eb46e38dc5d38cefdd07ee1b6395d
mrtong96 <mrtong96@gmail.com>
2021-07-18 19:14:25 -0700

    fixing the 403 error

--- data_collection_scripts/get_historical_data.py
+++ data_collection_scripts/get_historical_data.py
@@ -12,8 +12,9 @@ start_date = end_date - datetime.timedelta(days=365 * 2)
 
 def get_historical_data(ticker, start_date=start_date, end_date=end_date):
     try:
+        headers = headers = {'User-Agent': ''}
         return stock_info.get_data(
-            ticker, start_date=start_date, end_date=end_date, index_as_date = False, interval="1d")
+            ticker, start_date=start_date, end_date=end_date, index_as_date = False, interval="1d", headers=headers)
     except:
         # really don't do this, it's hacky but whatever
         return ('ERROR', ticker)
